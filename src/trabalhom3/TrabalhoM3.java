44c0bdc82994bcd859c3beb7e37135a2549db87e
unknown <5498465@U705635.univali.br>
2017-06-20 19:15:52 -0300

    Temp

--- src/trabalhom3/TrabalhoM3.java
+++ src/trabalhom3/TrabalhoM3.java
@@ -11,7 +11,7 @@ public class TrabalhoM3 {
     public static void main(String[] args) {
         ParametrosConexao parametros = new ParametrosConexao();                
         parametros.lerArquivoConfiguracao();        
-        GerenciadorDeDados gerenciadorDeDados = new GerenciadorDeDados(parametros);        
+        GerenciadorDeDados gerenciadorDeDados = new GerenciadorDeDados(parametros);
         gerenciadorDeDados.conecta();        
     }
     
